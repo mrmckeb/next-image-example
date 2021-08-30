fea102af6ad913b5f70f7a959393b2f8a483cf8e
Brody McKee <mrmckeb@gmail.com>
2021-08-30 12:55:48 +0300

    Add better image examples

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
 import Image from "next/image";
 
-const WIDTH = 1860 / 2;
-const HEIGHT = 616 / 2;
+const WIDTH = 1320 / 2;
+const HEIGHT = 570 / 2;
 
 export default function Home() {
   return (
     <div>
       <h3>Original</h3>
-      <p>1x/2x 59.4 kB</p>
+      <p>1x/2x 58.2 kB</p>
       <Image
         src="/static/example.png"
         unoptimized
@@ -16,7 +16,7 @@ export default function Home() {
         height={HEIGHT}
       />
       <h3>Default</h3>
-      <p>1x 12.3 kB / 2x 20.8 kB</p>
+      <p>1x 6.1 kB / 2x 11.4 kB</p>
       <Image
         src="/static/example.png"
         alt="Example"
@@ -24,7 +24,7 @@ export default function Home() {
         height={HEIGHT}
       />
       <h3>Quality 100</h3>
-      <p>1x 28.5 kB / 2x 42.6 kB</p>
+      <p>1x 13.8 kB / 2x 23.5 kB</p>
       <Image
         src="/static/example.png"
         quality={100}
@@ -33,7 +33,7 @@ export default function Home() {
         height={HEIGHT}
       />
       <h3>Lossless</h3>
-      <p>1x/2x 19.8 kB (effort: 9, slight loss: 0)</p>
+      <p>1x/2x 23.5 kB (effort: 9, slight loss: 0)</p>
       <Image
         src="/static/example.webp"
         unoptimized
